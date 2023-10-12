411e80030284b8e4ab90b7e073dd3551c786576f
sunil <sunilnitk354@gmail.com>
2023-10-12 13:19:26 +0530

    test commit

---
+++ src/main/java/com/earthworm/bankmgmtsolution/BankMgmtSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.earthworm.bankmgmtsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankMgmtSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankMgmtSolutionApplication.class, args);
+	}
+
+}
